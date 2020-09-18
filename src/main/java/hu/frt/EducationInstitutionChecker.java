a8fe7a91234236fcabd164619bcfb8a2adafe686
fratertamas <fratertomi@gmail.com>
2020-09-18 17:12:38 +0200

    initial commit

---
+++ src/main/java/hu/frt/EducationInstitutionChecker.java
@@ -0,0 +1,15 @@
+package hu.frt;
+
+import hu.frt.EducationalInstitution;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class EducationInstitutionChecker {
+
+}
