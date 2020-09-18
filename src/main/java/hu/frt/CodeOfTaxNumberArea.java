74a965b473decf7a9f3fdf286d68a07e5114d664
fratertamas <fratertomi@gmail.com>
2020-09-18 21:19:35 +0200

    add check

---
+++ src/main/java/hu/frt/CodeOfTaxNumberArea.java
@@ -0,0 +1,24 @@
+package hu.frt;
+
+import java.util.List;
+
+public class CodeOfTaxNumberArea {
+    private String dictname;
+    private List<TaxNumberArea> rows;
+
+    public String getDictname() {
+        return dictname;
+    }
+
+    public void setDictname(String dictname) {
+        this.dictname = dictname;
+    }
+
+    public List<TaxNumberArea> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<TaxNumberArea> rows) {
+        this.rows = rows;
+    }
+}
---
+++ src/main/java/hu/frt/TaxNumberArea.java
@@ -0,0 +1,23 @@
+package hu.frt;
+
+public class TaxNumberArea {
+    private String kod;
+
+    public String getKod() {
+        return kod;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
+
+    public String getTerulet() {
+        return terulet;
+    }
+
+    public void setTerulet(String terulet) {
+        this.terulet = terulet;
+    }
+
+    private String terulet;
+}
